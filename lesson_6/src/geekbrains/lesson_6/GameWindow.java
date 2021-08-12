ec55136383d75bf760a56ae671354dd32e3f30dc
Denis Khvorostin <denis.khvorostin@gmail.com>
2021-08-12 11:13:22 +0300

    lesson 6. catch the drop game: add images

--- lesson_6/src/geekbrains/lesson_6/GameWindow.java
+++ lesson_6/src/geekbrains/lesson_6/GameWindow.java
@@ -1,7 +1,9 @@
 package geekbrains.lesson_6;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 /**
  * Для создания окна используем стандартную библиотеку Swing.
@@ -10,22 +12,48 @@ import java.awt.*;
  */
 public class GameWindow extends JFrame {
 
+    // окно игры
     private static GameWindow game_window;
 
-    public static void main(String[] args) {
+    // время последнего кадра
+    private static long last_frame_time;
+
+    // изображения
+    private static Image background;
+    private static Image game_over;
+    private static Image space_invader;
+
+    // координаты захватчика
+    private static float space_invader_left = 200;
+    private static float space_invader_top = -100;
+    // скорость движения захватчика
+    private static float space_invader_v = 200;
+
+    public static void main(String[] args) throws IOException {
+
+        // подгружаем изображения
+        background = ImageIO.read(GameWindow.class.getResourceAsStream("img/background.jpg"));
+        game_over = ImageIO.read(GameWindow.class.getResourceAsStream("img/game_over.png"));
+        space_invader = ImageIO.read(GameWindow.class.getResourceAsStream("img/space_invader.png"));
+
         // создаем инстанс класса, сохраняем ссылку на него в переменной класса
         game_window = new GameWindow();
+
         // настраиваем окно
         game_window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // при закрытии окна программа завершается
         game_window.setLocation(200, 100); // начальная позиция окна
         game_window.setSize(906, 478); // размеры окна
         game_window.setResizable(false); // запрет на изменение размера окна
 
+        // фиксируем время первого кадра
+        last_frame_time = System.nanoTime();
+
         // создаём панель и добавляем её на окно
         GameField game_field = new GameField();
         game_window.add(game_field);
 
-        game_window.setVisible(true); // делаем окно видимым (по умолчанию, оно невидимо)
+        // делаем окно видимым (по умолчанию, оно невидимо)
+        game_window.setVisible(true);
     }
 
     /**
@@ -34,8 +62,25 @@ public class GameWindow extends JFrame {
      * @param  G
      */
     private static void onRepaint(Graphics G) {
-        // G.fillOval(10, 10, 200, 100);
-        G.drawLine(40, 40, 300, 200);
+
+        // текущее время в наносекундах
+        long current_time = System.nanoTime();
+
+        // определяем время между кадрами и переводим результат из наносекунд в секунды
+        float delta_time = (current_time - last_frame_time) * 0.000000001f;
+
+        // обновляем время предыдущего кадра
+        last_frame_time = current_time;
+
+        // рисуем фон
+        G.drawImage(background, 0, 0, null);
+
+        // рисуем захватчика
+        space_invader_top = space_invader_top + space_invader_v * delta_time;
+        space_invader_left = space_invader_left + space_invader_v * delta_time;
+        G.drawImage(space_invader, (int)space_invader_left, (int)space_invader_top, null);
+
+        // G.drawImage(game_over, 280, 50, null);
     }
 
     /**
@@ -58,8 +103,14 @@ public class GameWindow extends JFrame {
             // отрисовываем панель (это делает метод paintComponent() класса JPanel,
             // т.е. выполняем метод из родительского класса)
             super.paintComponent(G);
+
             // вызываем наш метод
             onRepaint(G);
+
+            // метод paintComponent() вызывается не постоянно, а только тогда, когда требуется
+            // отрисовать панель, что может происходить довольно редко. Делаем так, чтобы это
+            // происходило как можно чаще
+            repaint();
         }
     }
 }
